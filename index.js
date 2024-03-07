a3647e3bb8f2f42cfcb98164fc3ac2cd1394eecd
4m4nu4l4 <65728939+4m4nu4l4@users.noreply.github.com>
2024-03-07 19:17:49 -0300

    .

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs =  require('fs')
+/*const fs =  require('fs')
 const colors = require('colors')
 
 const callback = (err, data) => {
@@ -9,4 +9,24 @@ const callback = (err, data) => {
   }
 }
 
-fs.readFile('file.txt', 'utf8', callback)
+fs.readFile('file.txt', 'utf8', callback)*/
+
+const { express } = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/item', {request, Response} => {
+  response.json{item}
+})
+
+
+app.listen(2432, {} => {
+  console.log('Server running on port 2432')
+}
+
+/*postman*/
